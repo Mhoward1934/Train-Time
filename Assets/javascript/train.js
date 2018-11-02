8ed7864c6517586a38f5620eb986beee77755779
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-11-01 22:06:07 -0500

    updated javascript

--- Assets/javascript/train.js
+++ Assets/javascript/train.js
@@ -12,22 +12,22 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-//console.log(database);
-// function currentTrain() {
-//   var trainName = ["HTown", "Capital Express", "Ocean Beacon"];
-//   var destination = ["Houston", "Austin", "Galveston"];
-//   var firstTime = ["08:00", "13:00", "15:00"];
-//   var frequency = ["90", "60", "120"];
-//   //console.log(currentName);
-// }
+console.log(database);
+
+//initial value
+var name = "";
+var destination = "";
+var firstTime = "";
+var frequency = "";
 
 $("button").on("click", function (event) {
   event.preventDefault();
 
-  var name = $("#trainName").val().trim();
-  var destination = $("#destination").val().trim();
-  var firstTime = $("#firstTrainTime").val().trim();
-  var frequency = $("#frequency").val().trim();
+  //Grab values from the form
+  name = $("#trainName").val().trim();
+  destination = $("#destination").val().trim();
+  firstTime = $("#firstTrainTime").val().trim();
+  frequency = $("#frequency").val().trim();
 
   database.ref().push({
     name: name,
@@ -37,6 +37,27 @@ $("button").on("click", function (event) {
   });
 })
 
+// Firebase watcher .on("child_added"
+database.ref().on("child_added", function (snapshot) {
+  // storing the snapshot.val() in a variable for convenience
+  var sv = snapshot.val();
+
+  // Console.loging added train schedule
+  console.log(sv.name);
+  console.log(sv.destination);
+  console.log(sv.firstTime);
+  console.log(sv.frequency);
+
+  // Change the HTML to reflect
+  $("#column1").text(sv.name);
+  $("#column2").text(sv.destination);
+  $("#column3").text(sv.firstTime);
+  $("#column4").text(sv.frequency);
+
+  // Handle the errors
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
 //$(document).on("click", currentTrain);
 
 
